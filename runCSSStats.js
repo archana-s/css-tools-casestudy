5e561198cd791dbfb842887961666e50c05b11e2
Archana Sankaranarayanan <archana.june@gmail.com>
2018-01-07 15:34:00 -0800

    Update runCSSStats.js

--- runCSSStats.js
+++ runCSSStats.js
@@ -1,15 +1,15 @@
-var fs = require('fs')
-var postcss = require('postcss')
-var cssstats = require('cssstats')
+const fs = require('fs')
+const postcss = require('postcss')
+const cssstats = require('cssstats')
  
-var css = fs.readFileSync('./styles.css', 'utf8')
+const css = fs.readFileSync('./styles.css', 'utf8')
 postcss()
   .use(cssstats())
   .process(css)
   .then(function (result) {
     result.messages.forEach(function (message) {
       console.log(message)
-      console.log('Displaying all font size');
+      console.log('Displaying all font size')
       console.log('------------------------')
       console.log(message.stats.declarations.getAllFontSizes())
       console.log('Displaying Selectors and their specificity')
