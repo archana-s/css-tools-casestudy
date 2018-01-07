e82e217b517a089f779d4eb22a5a5f7c4c6b135c
Archana Sankaranarayanan <archana.june@gmail.com>
2018-01-07 15:42:32 -0800

    Update runAnalyzeCss.js

--- runAnalyzeCss.js
+++ runAnalyzeCss.js
@@ -1,6 +1,8 @@
-var fs = require('fs')
-var analyzer = require('analyze-css');
-var css = fs.readFileSync('./styles.css', 'utf8')
+const fs = require('fs')
+const analyzer = require('analyze-css');
+
+/* Include path to your CSS file here */
+const css = fs.readFileSync('./styles.css', 'utf8')
 
 new analyzer(css, function(err, results) {
   console.error(err);
