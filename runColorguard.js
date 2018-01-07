6b67cc49b7ff849ae4ef28b4daf7aed2ccd5a944
Archana Sankaranarayanan <archana.june@gmail.com>
2018-01-07 15:36:35 -0800

    Update runColorguard.js

--- runColorguard.js
+++ runColorguard.js
@@ -1,6 +1,6 @@
-var fs = require('fs')
-var colorguard = require('colorguard')
-var css = fs.readFileSync('./styles.css', 'utf8')
+const fs = require('fs')
+const colorguard = require('colorguard')
+const css = fs.readFileSync('./styles.css', 'utf8')
 
 colorguard.process(css, []).then(function(result) {
   // process results or integrate in linting or development workflow
